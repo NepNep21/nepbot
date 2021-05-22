81d078bbcf4da3cadd1b88e9909d2cd8a347d01d
Lolicon2110 <43792621+Lolicon2110@users.noreply.github.com>
2021-05-22 19:20:43 -0300

    More open source preparations

--- src/main/java/me/nepnep/nepbot/message/command/commands/Shutdown.java
+++ src/main/java/me/nepnep/nepbot/message/command/commands/Shutdown.java
@@ -1,29 +1,33 @@
 package me.nepnep.nepbot.message.command.commands;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import me.nepnep.nepbot.message.command.Category;
 import me.nepnep.nepbot.message.command.ICommand;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 public class Shutdown implements ICommand {
     @Override
     public void execute(List<String> args, GuildMessageReceivedEvent event) {
-        TextChannel channel = event.getChannel();
-        if (event.getAuthor().getIdLong() == 264171415614259200L) {
-            channel.sendMessage("Shutting down").complete();
-            event.getJDA().shutdown();
-            // Try-catch and sleep may not be necessary
-            try {
-                Thread.sleep(2000);
+        try {
+            long operator = new ObjectMapper().readTree(new File("config.json")).get("operator").longValue();
+            TextChannel channel = event.getChannel();
+            if (event.getAuthor().getIdLong() == operator) {
+                channel.sendMessage("Shutting down").complete();
+                event.getJDA().shutdown();
                 System.exit(0);
-            } catch (InterruptedException e) {
-                // Intentionally empty
+            } else {
+                channel.sendMessage("You do not have permission to use this command").queue();
             }
-        } else {
-            channel.sendMessage("You do not have permission to shut the bot down!").queue();
+        } catch (IOException e) {
+            LoggerFactory.getLogger(Shutdown.class).error("IOException at Shutdown", e);
         }
     }
 
