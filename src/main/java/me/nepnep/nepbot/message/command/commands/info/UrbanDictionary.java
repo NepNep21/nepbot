3ecaafcc707a98c18b7806d08470b2f942b702d8
Lolicon2110 <43792621+Lolicon2110@users.noreply.github.com>
2021-05-22 18:51:24 -0300

    Improve UrbanDictionary and prepare for open sourcing

--- src/main/java/me/nepnep/nepbot/message/command/commands/info/UrbanDictionary.java
+++ src/main/java/me/nepnep/nepbot/message/command/commands/info/UrbanDictionary.java
@@ -1,6 +1,6 @@
 package me.nepnep.nepbot.message.command.commands.info;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import me.nepnep.nepbot.message.command.Category;
 import me.nepnep.nepbot.message.command.ICommand;
@@ -8,9 +8,9 @@ import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
-import okhttp3.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.ResponseBody;
 
 import java.io.IOException;
 import java.net.URLEncoder;
@@ -18,43 +18,42 @@ import java.util.List;
 
 public class UrbanDictionary implements ICommand {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(UrbanDictionary.class);
-
     @Override
     public void execute(List<String> args, GuildMessageReceivedEvent event) {
         TextChannel channel = event.getChannel();
         if (args.size() >= 1) {
-            String api = "http://urbanscraper.herokuapp.com/";
+            String api = "http://api.urbandictionary.com/v0/define?term=";
             String query = String.join(" ", args.subList(0, args.size()));
             ResponseBody body = null;
             try {
-                String safeQuery = "define/" + URLEncoder.encode(query, "UTF-8");
+                String safeQuery = URLEncoder.encode(query, "UTF-8");
                 OkHttpClient client = new OkHttpClient();
                 Request request = new Request.Builder().url(api + safeQuery).build();
-                Response response = client.newCall(request).execute();
-                body = response.body();
+                body = client.newCall(request).execute().body();
                 ObjectMapper mapper = new ObjectMapper();
 
-                if (response.code() == 200) {
-                    JSONObject json = mapper.readValue(body.string(), JSONObject.class);
+                JsonNode json = mapper.readTree(body.string());
+                JsonNode best = json.get("list").get(0);
 
-                    String definition = json.getDefinition();
-                    String safeDefinition = definition.replace("nig", "no");
-                    String example = json.getExample();
-                    String safeExample = example.replace("nig", "no");
-
-                    EmbedBuilder builder = new EmbedBuilder();
-
-                    builder.addField("Definition:", safeDefinition, false);
-                    builder.addField("Example:", safeExample, false);
-
-                    channel.sendMessage(builder.build()).queue();
-                } else {
-                    channel.sendMessage("Nothing found!").queue();
+                if (best == null) {
+                    body.close();
+                    channel.sendMessage("Not found").queue();
+                    return;
                 }
 
+                String definition = best.get("definition").asText();
+                String example = best.get("example").asText();
+
+                String safeDefinition = definition.replaceAll("nigg|fag", "[Removed]");
+                String safeExample = example.replaceAll("nigg|fag", "[Removed]");
+
+                EmbedBuilder builder = new EmbedBuilder();
+                builder.addField("Definition:", safeDefinition, false);
+                builder.addField("Example:", safeExample, false);
+
+                channel.sendMessage(builder.build()).queue();
             } catch (IOException e) {
-                LOGGER.error("An exception occurred at UrbanDictionary!", e);
+                channel.sendMessage("API Issues, try again later.").queue();
             } finally {
                 if (body != null) {
                     body.close();
@@ -62,20 +61,6 @@ public class UrbanDictionary implements ICommand {
             }
         } else {
             channel.sendMessage("Invalid usage!").queue();
-        }
-    }
-
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    private static class JSONObject {
-        String definition;
-        String example;
-
-        public String getExample() {
-            return example;
-        }
-
-        public String getDefinition() {
-            return definition;
         }
     }
 
