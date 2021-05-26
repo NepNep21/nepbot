81af2a2f751da94edadd91153ae6febb4962c542
Lolicon2110 <43792621+Lolicon2110@users.noreply.github.com>
2021-05-26 09:21:35 -0300

    Improve BotInfo
    Remove CPU usage to stop using unsafe packages
    Consistent capitalization

--- src/main/java/me/nepnep/nepbot/message/command/commands/info/BotInfo.java
+++ src/main/java/me/nepnep/nepbot/message/command/commands/info/BotInfo.java
@@ -1,6 +1,5 @@
 package me.nepnep.nepbot.message.command.commands.info;
 
-import com.sun.management.OperatingSystemMXBean;
 import me.nepnep.nepbot.message.command.Category;
 import me.nepnep.nepbot.message.command.ICommand;
 import net.dv8tion.jda.api.EmbedBuilder;
@@ -17,26 +16,22 @@ public class BotInfo implements ICommand {
     public void execute(List<String> args, GuildMessageReceivedEvent event) {
         List<Guild> guilds = event.getJDA().getGuilds();
         Runtime runtime = Runtime.getRuntime();
-        OperatingSystemMXBean bean = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class);
         EmbedBuilder builder = new EmbedBuilder();
 
         String javaVersion = System.getProperty("java.version");
         int guildAmount = guilds.size();
         int userAmount = guilds.stream().mapToInt(Guild::getMemberCount).sum();
         long memoryUsed = runtime.totalMemory() - runtime.freeMemory();
-        double cpuUsage = bean.getProcessCpuLoad();
         long uptime = ManagementFactory.getRuntimeMXBean().getUptime();
         String jdaVersion = JDAInfo.VERSION;
 
-        builder.addField("Java version", javaVersion, true);
+        builder.addField("Java Version", javaVersion, true);
 
-        builder.addField("Total guilds", String.valueOf(guildAmount), true);
+        builder.addField("Total Guilds", String.valueOf(guildAmount), true);
 
-        builder.addField("Total users", String.valueOf(userAmount), true);
+        builder.addField("Total Users", String.valueOf(userAmount), true);
 
-        builder.addField("RAM Usage", memoryUsed / 1000000 + "M", true);
-
-        builder.addField("CPU Usage", Math.round(cpuUsage) + "%", true);
+        builder.addField("RAM Usage", memoryUsed / 1000000 + "MB", true);
 
         builder.addField("Uptime", uptime / 1000 / 60 + " Minutes", true);
 
