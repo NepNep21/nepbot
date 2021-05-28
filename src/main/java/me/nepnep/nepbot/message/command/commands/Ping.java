874e4ded2c964a1f02d1475aedc39fd352692bec
Lolicon2110 <43792621+Lolicon2110@users.noreply.github.com>
2021-05-28 08:56:23 -0300

    Use getGatewayPing()

--- src/main/java/me/nepnep/nepbot/message/command/commands/Ping.java
+++ src/main/java/me/nepnep/nepbot/message/command/commands/Ping.java
@@ -3,23 +3,14 @@ package me.nepnep.nepbot.message.command.commands;
 import me.nepnep.nepbot.message.command.Category;
 import me.nepnep.nepbot.message.command.ICommand;
 import net.dv8tion.jda.api.Permission;
-import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
-import net.dv8tion.jda.api.exceptions.ErrorResponseException;
 
 import java.util.List;
 
 public class Ping implements ICommand {
     @Override
     public void execute(List<String> args, GuildMessageReceivedEvent event) {
-        long time = System.currentTimeMillis();
-        TextChannel channel = event.getChannel();
-
-        try {
-            channel.sendMessage("Pong!").queue(response -> response.editMessageFormat("Pong: %d%s", System.currentTimeMillis() - time, "ms").queue());
-        } catch (ErrorResponseException e) {
-            // Intentionally empty
-        }
+        event.getChannel().sendMessageFormat("Pong: %d%s", event.getJDA().getGatewayPing(), "ms").queue();
     }
     @Override
     public String getInvoke() {
