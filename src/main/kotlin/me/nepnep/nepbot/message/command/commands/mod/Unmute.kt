b5c282b397e4836b870fc1733881ab8de6fd9a55
NepNep21 <43792621+NepNep21@users.noreply.github.com>
2022-01-15 17:53:32 -0300

    Make Unmute more consistent and work with timeouts

--- src/main/kotlin/me/nepnep/nepbot/message/command/commands/mod/Unmute.kt
+++ src/main/kotlin/me/nepnep/nepbot/message/command/commands/mod/Unmute.kt
@@ -1,57 +1,41 @@
 package me.nepnep.nepbot.message.command.commands.mod
 
-import me.nepnep.nepbot.message.command.Category
 import me.nepnep.nepbot.message.command.AbstractCommand
+import me.nepnep.nepbot.message.command.Category
 import net.dv8tion.jda.api.Permission
 import net.dv8tion.jda.api.entities.GuildMessageChannel
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
-import net.dv8tion.jda.api.exceptions.HierarchyException
 
 class Unmute : AbstractCommand(
     "unmute",
     Category.MOD,
     "Unmutes someone: ;unmute <Mention member>",
-    Permission.KICK_MEMBERS
+    Permission.MODERATE_MEMBERS
 ) {
     override fun execute(args: List<String>, event: MessageReceivedEvent, channel: GuildMessageChannel) {
         val mentioned = event.message.mentionedMembers
 
         if (mentioned.isEmpty()) {
-            channel.sendMessage(":x: Incorrect usage").queue()
+            channel.sendMessage(":x: Incorrect usage.").queue()
             return
         }
         if (mentioned.size > 1) {
-            channel.sendMessage(":x: Please only mention one user.").queue()
+            channel.sendMessage(":x: Please only mention one member.").queue()
             return
         }
         val toUnmute = mentioned[0]
-        val guild = event.guild
-
-        if (!event.member!!.canInteract(toUnmute)) {
-            channel.sendMessage(":x: You don't have permission to use this command.").queue()
+        if (!toUnmute.isTimedOut) {
+            channel.sendMessage(":x: Member is not timed out.").queue()
             return
         }
 
-        val selfMember = guild.selfMember
-        if (!selfMember.hasPermission(Permission.MANAGE_ROLES) || !selfMember.canInteract(toUnmute)) {
-            channel.sendMessage(":x: I can't manage roles that user or I don't have permission to manage roles.")
-                .queue()
+        val selfMember = event.guild.selfMember
+        if (!selfMember.hasPermission(Permission.MODERATE_MEMBERS) || !selfMember.canInteract(toUnmute)) {
+            channel.sendMessage(":x: I can't unmute that user or I don't have permission to unmute.").queue()
             return
         }
 
-        val roles = guild.getRolesByName("muted", true)
-
-        if (roles.isEmpty()) {
-            channel.sendMessage(":x: Unable to find role named \"Muted\" (Not case sensitive)").queue()
-            return
-        }
-        val muted = roles[0]
-
-        try {
-            guild.removeRoleFromMember(toUnmute, muted).queue()
-            channel.sendMessage("Unmuted ${toUnmute.user.asTag}").queue()
-        } catch (e: HierarchyException) {
-            channel.sendMessage(":x: I can't manage the Muted role, it is higher than my max role!").queue()
-        }
+        toUnmute.removeTimeout().queue()
+        channel.sendMessage("Unmuted ${toUnmute.user.asTag}.").queue()
     }
 }
